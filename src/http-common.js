199046269b06d15398f431715da69851642ebdc9
Vishal <vishalchauhan3549@gmail.com>
2023-12-28 17:11:55 +0530

    Changed baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
+// export default axios.create({
+//   baseURL: "http://localhost:8080/api",
+//   headers: {
+//     "Content-type": "application/json"
+//   }
+// });
+
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
+    baseURL: "http://54.80.12.24:8080/api",
+    headers: {
+        "Content-type": "application/json",
+    },
+})
