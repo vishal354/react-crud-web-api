4fa349ca02f905e12606a29e87ddc38e25bcbdcc
Vishal <vishalchauhan3549@gmail.com>
2024-01-02 15:42:23 +0530

    Updated the laod balancer URL

--- src/http-common.js
+++ src/http-common.js
@@ -7,8 +7,10 @@ import axios from "axios";
 //   }
 // });
 
+const loadBalancerURL = "http://tutorial-lb-1671621252.us-east-1.elb.amazonaws.com/api"
+
 export default axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: loadBalancerURL,
     headers: {
         "Content-type": "application/json",
     },
